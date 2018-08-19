ee09e1ed3f4ae582f037c290a34ac83651ea3c03
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-19 23:41:28 +0530

    Update Client.java

--- Client.java
+++ Client.java
@@ -20,6 +20,54 @@ public class Client {
     int proxyport =Integer.valueOf(args[2]).intValue; 
     String name = args[3] ; 
     
+    // create instance of RMI to connect to registory 
+    RMI rmi = null ;
+    RemoteObjectRefrence ror = null;
+    try{
+      rmi = new RMI (host,port, proxyport);
+      ror = rmi.lookup(name);
+    } catch (Exception e){
+      System.out.println(e);
+      System.out.println("either arguments incorrect or name not in registory ");
+      if(rmi!=null){
+        rmi.close();
+        return ;
+      }
+      MathInter maths = null;
+      try{
+        maths = (MathInter)ror.localise();
+      }catch(Exception e){
+        System.out.println("localise has thrown exception");
+        System.out.println(e);
+        return ; 
+      }
+    
+         if(!maths.contains("a")) {
+            maths.newVar("a", (Integer)3);
+        }
+        if(!maths.contains("b")) {
+            maths.newVar("b", (Integer)6);
+        }
+        if(!maths.contains("c")) {
+            maths.newVar("c", (Integer)69);
+        }
+
+        maths.addTo("a", maths.getValue("b"));
+        maths.addTo("c", maths.getValue("a"));
+
+        System.out.println("The value of a is " + maths.getValue("a"));
+        System.out.println("The value of b is " + maths.getValue("b"));
+        System.out.println("The value of c is " + maths.getValue("c"));
+
+        maths.remove("b");
+
+        // prints all variables on remote end
+        maths.printAll();
+
+        rmi.close();
+        return;
+    }
+}
     
     
   
