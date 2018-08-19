7c6d04e4224a67da2e9196daa7e42652cf7ba4a6
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-19 23:08:23 +0530

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,1 +1,22 @@
 import java.io.*;
+import java.util.*;
+
+public class Client {
+  // takes these as argument 
+  // host
+  //port
+  //proxy port to listen on 
+  //name of object to use
+  
+  
+  public static void main (String args[])throws IOException{
+    if(args.length!= 4 ) {
+      System.out.println("insufficient arguments, requires :  ");
+       System.out.println ("<host><port><proxyport > " + "<objectname>");
+      return;
+    }
+    
+    
+    
+  
+    
